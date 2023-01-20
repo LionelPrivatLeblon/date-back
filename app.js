7073eda1b0578ca4d012e80f291ef4227f895c1e
Lionel <l.privatleblon@gmail.com>
2023-01-20 15:30:51 +0100

    Sauvegarde

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var yearRouter = require("./routes/index");
 const cors = require("cors");
 
 var app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +20,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/year", yearRouter);
-app.use(cors());
 
 module.exports = app;
